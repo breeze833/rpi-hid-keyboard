2323c48b5dd6dd9f403c077dd3c17d9474a4bb14
Feng-Cheng Chang <breeze833@gmail.com>
2026-05-30 07:20:21 +0800

    Add signal handler to ensure proper cleaup

--- src/rpi_hid/cli.py
+++ src/rpi_hid/cli.py
@@ -1,5 +1,15 @@
 import os
+import signal
 from rpi_hid import HIDDaemon, HIDInputSrc, HIDOutputDst, Str2HIDTransformer
+
+def service_shutdown_handler(signum, frame):
+    # Raising KeyboardInterrupt forces Python to bubble up cleanly 
+    # through all try/finally blocks and Context Managers.
+    raise KeyboardInterrupt
+
+# Register the hooks at the very entry point of your main script
+signal.signal(signal.SIGTERM, service_shutdown_handler)
+signal.signal(signal.SIGINT, service_shutdown_handler) # Handles Ctrl+C too
 
 def main():
     socket_path = os.getenv('HID_SOCKET_PATH', '/tmp/hid_keyboard.sock')
